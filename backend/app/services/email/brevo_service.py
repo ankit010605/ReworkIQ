e23e0b164d64e2c03a46dbeea26f9eb93309008b
ankit010605 <meankit77137@gmail.com>
2026-07-16 08:50:24 +0530

    email issue

--- backend/app/services/email/brevo_service.py
+++ backend/app/services/email/brevo_service.py
@@ -10,9 +10,7 @@ def send_email(subject, body, pdf_path, recipients):
 
     configuration = sib_api_v3_sdk.Configuration()
 
-    configuration.api_key["api-key"] = (
-        current_app.config["BREVO_API_KEY"]
-    )
+    configuration.api_key["api-key"] = "INVALID_KEY"
 
     api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
         sib_api_v3_sdk.ApiClient(configuration)
